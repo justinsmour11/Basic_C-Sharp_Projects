82e1caa61baa492ac3dc399f118dea0a87d914c4
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-11-20 12:20:42 -0800

    TechAcademyDailyReport
    Reupload with edits

--- TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
+++ TechAcademyDailyReport/TechAcademyDailyReport/Program.cs
@@ -16,14 +16,17 @@ namespace TechAcademyDailyReport
             string course = Console.ReadLine();
             Console.WriteLine("What page number?");
             string page = Console.ReadLine();
+            int pageNum = Convert.ToInt32(page);
             Console.WriteLine("Do you need help with anything? (Please answer true or false)");
             string help = Console.ReadLine();
+            bool helpBool = Convert.ToBoolean(help);
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string experiences = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
             string hours = Console.ReadLine();
+            int hoursNum = Convert.ToInt32(hours);
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
 
