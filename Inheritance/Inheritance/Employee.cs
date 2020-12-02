55632b543a9fbaf01ee19ddaadaab2e3cd6692d7
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-12-02 14:20:31 -0800

    Generic Types

--- Inheritance/Inheritance/Employee.cs
+++ Inheritance/Inheritance/Employee.cs
@@ -6,18 +6,20 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    public class Employee : Person
+    public class Employee<T> : Person
     {
+        public List<T> Things { get; set; }
+
         public int ID { get; set; }
 
-        public static bool operator ==(Employee employee1, Employee employee2)
-        {
-            return (employee1.ID == employee2.ID);
-        }
-        public static bool operator !=(Employee employee1, Employee employee2)
-        {
-            return !(employee1.ID == employee2.ID);
-        }
+        //public static bool operator ==(Employee employee1, Employee employee2)
+        //{
+        //    return (employee1.ID == employee2.ID);
+        //}
+        //public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    return !(employee1.ID == employee2.ID);
+        //}
     }
     
     
