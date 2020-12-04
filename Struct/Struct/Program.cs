e9cdd30b82050cc551890e6e5f79314ea7a59e46
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-12-04 11:16:16 -0800

    Struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 10.00m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
