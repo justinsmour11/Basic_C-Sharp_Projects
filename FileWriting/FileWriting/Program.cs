0d500332cbbfe245be32c4b85466015142b32ec7
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-12-07 15:01:43 -0800

    Writing to and reading from files

---
+++ FileWriting/FileWriting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter a number:");
+            File.WriteAllText(@"C:\Users\justi\Desktop\myDoc.txt", Console.ReadLine());
+            string text = File.ReadAllText(@"C:\Users\justi\Desktop\myDoc.txt");
+            Console.WriteLine(text);
+            Console.Read();
+        }
+    }
+}
