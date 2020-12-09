d9d8c67b896e2a2d13b14bc47a0b493af70ccb45
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-12-09 09:58:10 -0800

    Constant, var, and constructor chain

---
+++ Constant/Constant/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    public class Employee
+    {
+        public Employee(string name) : this(name, 40)
+        {
+
+        }
+        public Employee(string name, int hours)
+        {
+            Name = name;
+            hourWorked = hours;
+        }
+        public string Name { get; set; }
+        public int hourWorked { get; set; }
+    }
+}
---
+++ Constant/Constant/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Justin";
+            var age = 31;
+            var employee = new Employee("Justin");
+            Console.WriteLine(employee.hourWorked);
+            Console.ReadLine();
+        }
+    }
+}
