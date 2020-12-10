54c943e1f0ef778527d54492a1b8cdecb023b419
justinsmour11 <68407200+justinsmour11@users.noreply.github.com>
2020-12-09 16:29:24 -0800

    More exceptions

---
+++ MoreExceptions/MoreExceptions/BelowZeroException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreExceptions
+{
+    public class BelowZeroException : Exception
+    {
+        public BelowZeroException()
+        { 
+            Console.WriteLine("Your age must be above zero!");
+            Console.Read();
+        }
+    }
+}
---
+++ MoreExceptions/MoreExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What is your age?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                if(age <= 0)
+                {
+                    throw new BelowZeroException();
+                }
+                int currentYear = 2020;
+                int yearBorn = currentYear - age;
+                Console.WriteLine("You were born in {0}", yearBorn);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a number");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
